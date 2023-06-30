c7028830153646d65459b7d25be5338d0d396fb9
Diana1Illescas <132688468+Diana1Illescas@users.noreply.github.com>
2023-06-30 10:34:25 -0500

    Add files via upload

---
+++ POO_U2_P11_SOBREESRITURA/src/main/java/com/mycompany/poo_u2_p11_sobreesritura/POO_U2_P11_SOBREESRITURA.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.poo_u2_p11_sobreesritura;
+public class POO_U2_P11_SOBREESRITURA {
+
+    public static void main(String[] args) {
+        Persona per1=new Persona ();
+        Gato gato1=new Gato();
+         
+        per1.comer();
+        gato1.comer();
+        
+    }
+}
---
+++ POO_U2_P11_SOBREESRITURA/src/main/java/com/mycompany/poo_u2_p11_sobreesritura/Persona.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo_u2_p11_sobreesritura;
+import Animal.Animal;
+
+public class Persona extends Animal{
+public void Comer(){
+    System.out.println("Soy un humano mi IQ es 150"); //coeficiente intelectual
+    }
+}
