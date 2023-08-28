78d5e21d36866ad85c92764cf05182e8600dd888
Diana1Illescas <132688468+Diana1Illescas@users.noreply.github.com>
2023-08-27 19:50:40 -0500

    Add files via upload

---
+++ lab2Diana_Horarios/src/horarios/Laboratorio.java
@@ -0,0 +1,39 @@
+package horarios;
+
+import java.util.List;
+import java.util.Set;
+
+public class Laboratorio {
+    private String nombre;
+    private int capacidad;
+    private Set<String> equipos;
+
+    public Laboratorio(String nombre, int capacidad, Set<String> equipos) {
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.equipos = equipos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public Set<String> getEquipos() {
+        return equipos;
+    }
+
+    public boolean verificarDisponibilidad(String horario, List<Asignatura> asignaturas) {
+        for (Asignatura asignatura : asignaturas) {
+            if (asignatura.getHorario().equals(horario) && asignatura.getLaboratorio().equals(this)) {
+                return false; // El laboratorio ya tiene asignada una asignatura en ese horario
+            }
+        }
+        return true; // El laboratorio está disponible en ese horario
+    }
+
+}
+
